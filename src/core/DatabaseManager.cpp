92e0a5908fc0d0504cdbb7bcc10dcc8ef9529085
kamerado <kamerado9@gmail.com>
2024-08-22 16:11:46 -0400

    fixed remove db bug

--- src/core/DatabaseManager.cpp
+++ src/core/DatabaseManager.cpp
@@ -148,60 +148,9 @@ void DatabaseManager::closeDatabase() {
     }
 
     // Remove the connection from the connection pool
-    QString connectionName = this->db.connectionName();
-    QSqlDatabase::removeDatabase(connectionName);
+    QSqlDatabase::removeDatabase(this->dbPath);
     qDebug() << "Database connection removed.";
 }
-// DatabaseManager::DatabaseManager(DatabaseManager&& other) noexcept : dbPath(std::move(other.dbPath)) {
-//     other.db = nullptr;
-//     other.isOpen = false;
-// }
-
-// DatabaseManager& DatabaseManager::operator=(DatabaseManager&& other) noexcept {
-//     if (this != &other) {
-//         dbPath = std::move(other.dbPath);
-//         db = other.db;
-//         isOpen = other.isOpen;
-//         other.db = nullptr;
-//         other.isOpen = false;
-//     }
-//     return *this;
-// }
-
-// bool DatabaseManager::open() {
-    
-// }
-
-// void DatabaseManager::Close() {
-//     if (isOpen) {
-//         db.close();
-//         isOpen = false;
-//     }
-// }
-
-// bool DatabaseManager::execute(const std::string& sql) {
-//     // char* errMsg = nullptr;
-//     // int result = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
-//     // if (result != SQLITE_OK) {
-//     //     std::cerr << "SQL error:  " << errMsg << std::endl;
-//     //     sqlite3_free(errMsg);
-//     //     return false;
-//     // }
-//     return true;
-// }
-
-
-
-// int DatabaseManager::callback(void* data, int argc, char** argv, char** azColName) {
-//     // auto* results = static_cast<std::vector<std::vector<std::string>>*>(data);
-//     // results->emplace_back();
-//     // std::vector<std::string>& row = results->back();
-//     // for (int i = 0; i < argc; i++) {
-//     //     // If the column value is NULL, add empty string to row
-//     //     row.push_back(argv[i] ? argv[i] : "");
-//     // }
-//     return 0;
-// }
 
 void DatabaseManager::testFunctionality() {
     std::cout << "Database manager responding." << std::endl;
