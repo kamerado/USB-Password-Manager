f18c229f4f516716565b86d6b3fa5071502f26df
kamerado <kamerado9@gmail.com>
2024-10-31 00:04:13 -0400

    update enc util

--- src/core/EncryptionUtil.cpp
+++ src/core/EncryptionUtil.cpp
@@ -1,11 +1,11 @@
-#include "crypto++/cryptlib.h"
-#include "crypto++/rijndael.h"
-#include "crypto++/modes.h"
-#include "crypto++/files.h"
-#include "crypto++/osrng.h"
-#include "crypto++/hex.h"
-#include "crypto++/default.h"
 #include "EncryptionUtil.h"
+#include "crypto++/cryptlib.h"
+#include "crypto++/default.h"
+#include "crypto++/files.h"
+#include "crypto++/hex.h"
+#include "crypto++/modes.h"
+#include "crypto++/osrng.h"
+#include "crypto++/rijndael.h"
 #include <filesystem>
 
 #include <string>
@@ -13,23 +13,23 @@
 using namespace CryptoPP;
 namespace fs = std::filesystem;
 
-EncryptionUtil::EncryptionUtil(std::string& pw) {
-  const char* encP =  strdup(pw.c_str());
+EncryptionUtil::EncryptionUtil(std::string &pw) {
+  const char *encP = strdup(pw.c_str());
   this->pw = encP;
 }
 
-EncryptionUtil::~EncryptionUtil() {
-}
+EncryptionUtil::~EncryptionUtil() {}
 
 void EncryptionUtil::EncryptFile() {
-  FileSource f(this->dbPath, true, new DefaultEncryptorWithMAC(this->pw,
-        new FileSink(this->dbePath)));
+  FileSource f(
+      this->dbPath, true,
+      new DefaultEncryptorWithMAC(this->pw, new FileSink(this->dbePath)));
   fs::remove(this->dbPath);
 }
 
 void EncryptionUtil::DecryptFile() {
-  std::cout << this->pw << std::endl;
-  FileSource f(this->dbePath, true, new DefaultDecryptorWithMAC(this->pw,
-        new FileSink(this->dbPath)));
+  FileSource f(
+      this->dbePath, true,
+      new DefaultDecryptorWithMAC(this->pw, new FileSink(this->dbPath)));
   fs::remove(this->dbePath);
 }
